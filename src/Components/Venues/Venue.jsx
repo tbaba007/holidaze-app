c10ca68180d10c8a17146cc2ec7fcc22f2236847
Anthony Okonta <okontaa@gmail.com>
2023-05-20 11:54:43 +0200

    chore: integrate login

--- src/Components/Venues/Venue.jsx
+++ src/Components/Venues/Venue.jsx
@@ -1,59 +1,31 @@
 import Button from '../Button';
 import venueStyles from "./Venue.module.scss";
-import sampleImage from '../../assets/images/sampleImage.svg'
-
-const stubVenue = [
-  {
-    id: 1,
-    address: "13th Avenue, Lisbay Street, Graz",
-    duration: "March 2-10",
-    price: "1100 KR/Night",
-    features: "4Beds 4Baths",
-    image:sampleImage
-  },
-  {
-    id: 2,
-    address: "13th Avenue, Lisbay Street, Graz",
-    duration: "March 2-10",
-    price: "1100 KR/Night",
-    features: "4Beds 4Baths",
-    image:sampleImage
-  },
-  {
-    id: 3,
-    address: "13th Avenue, Lisbay Street, Graz",
-    duration: "March 2-10",
-    price: "1100 KR/Night",
-    features: "4Beds 4Baths",
-    image:sampleImage
-  },
-  
-];
 
 const Venue = ({venuList}) => {
   return (
     <>
       <section className={venueStyles.container__wrapper}>
         {venuList.slice(7,10).map((item) => {
+          const {address}=item.location
           return (
             <section className={venueStyles.container} key={item.id}>
                 <img src={item?.media[0]} alt={item?.media[0]} className={venueStyles.container__img}/>
-              {/* <label className={venueStyles.container__header}>
-                {item.address}
+              <label className={venueStyles.container__header}>
+              &nbsp;  &nbsp;{address}
               </label>
-              <label className={venueStyles.container__duration}>
+              {/* <label className={venueStyles.container__duration}>
                 {item.duration}
-              </label>
-              <section className={venueStyles.container__features}>
+              </label> */}
+              {/* <section className={venueStyles.container__features}>
                 {
                     item.features.split(' ').map((features,index)=>{
                         return <label key={index+features}>{features}</label>
                     })
                 }
-              </section>
+              </section> */}
               <label className={venueStyles.container__price}>
-                {item.price}
-              </label> */}
+                &nbsp;   {item.price}
+              </label>
               <section className={venueStyles.container__footer}>
                 <button className={venueStyles.container__viewDetails}>View Details</button>
                 <Button text="Book Now"></Button>
